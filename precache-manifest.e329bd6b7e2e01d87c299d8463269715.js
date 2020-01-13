6aa17c97c7d03a3df1ce7288daa9caa4f584ec3d
Alfred <alfred@bufferapp.com>
2020-01-13 09:34:05 +0800

    Updates

---
+++ precache-manifest.e329bd6b7e2e01d87c299d8463269715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f55e8968b7227b6b76fbd2dce7fddf3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "22168ee7a4d1e2b386d0",
+    "url": "/react-tutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "88af68aa040d5b2c0dc8",
+    "url": "/react-tutorial/static/js/2.c950ad14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.c950ad14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22168ee7a4d1e2b386d0",
+    "url": "/react-tutorial/static/js/main.9c9726ca.chunk.js"
+  },
+  {
+    "revision": "da5c02f9a44783ad6b5b",
+    "url": "/react-tutorial/static/js/runtime-main.d2b0c93e.js"
+  }
+]);
